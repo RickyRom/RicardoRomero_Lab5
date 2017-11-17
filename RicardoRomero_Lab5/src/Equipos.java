a2fb8132389a06faa4ad1bd635c8b47934eeb568
RickyRom <30517351+RickyRom@users.noreply.github.com>
2017-11-17 13:25:19 -0600

    avance de la clase equipos

--- RicardoRomero_Lab5/src/Equipos.java
+++ RicardoRomero_Lab5/src/Equipos.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,64 @@
  * @author ricky
  */
 public class Equipos {
-    
+    private String nombre;
+    private double presupuesto;
+    private int numero_copas;
+    private String estadio;
+    ArrayList<Jugadores> j = new ArrayList();
+
+    public Equipos() {
+    }
+
+    public Equipos(String nombre, double presupuesto, int numero_copas, String estadio) {
+        this.nombre = nombre;
+        this.presupuesto = presupuesto;
+        this.numero_copas = numero_copas;
+        this.estadio = estadio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPresupuesto() {
+        return presupuesto;
+    }
+
+    public void setPresupuesto(double presupuesto) {
+        this.presupuesto = presupuesto;
+    }
+
+    public int getNumero_copas() {
+        return numero_copas;
+    }
+
+    public void setNumero_copas(int numero_copas) {
+        this.numero_copas = numero_copas;
+    }
+
+    public String getEstadio() {
+        return estadio;
+    }
+
+    public void setEstadio(String estadio) {
+        this.estadio = estadio;
+    }
+
+    public ArrayList<Jugadores> getJ() {
+        return j;
+    }
+
+    public void setJ(ArrayList<Jugadores> j) {
+        this.j = j;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
 }
